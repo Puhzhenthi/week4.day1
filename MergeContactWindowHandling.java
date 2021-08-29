62cf3d87e451801275dc8f7a446ea9053a1c36b3
Puhzhenthi <88572023+Puhzhenthi@users.noreply.github.com>
2021-08-29 13:20:08 +0530

    Add files via upload

---
+++ MergeContactWindowHandling.java
@@ -0,0 +1,55 @@
+package week4.day1.Assignments;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MergeContactWindowHandling {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromiumdriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		String title = driver.getTitle();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		WebElement webUser = driver.findElement(By.id("username"));
+		webUser.sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Contacts")).click();
+		driver.findElement(By.linkText("Merge Contacts")).click();
+		driver.findElement(By.xpath("(//img[@alt='Lookup'])[1]")).click();
+		Set<String> windowHandlesSet = driver.getWindowHandles();
+		List<String> windowHandlesList = new ArrayList<String>(windowHandlesSet);
+		driver.switchTo().window(windowHandlesList.get(1));
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId'][1]/a")).click();
+		driver.switchTo().window(windowHandlesList.get(0));
+		driver.findElement(By.xpath("//input[@id='partyIdTo']/following::a[1]/img")).click();
+		Set<String> windowHandlesSet2 = driver.getWindowHandles();
+		List<String> windowHandlesList2 = new ArrayList<String>(windowHandlesSet2);
+		driver.switchTo().window(windowHandlesList2.get(1));
+		((RemoteWebDriver) driver).findElementByXPath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[2]/a").click();
+		driver.switchTo().window(windowHandlesList2.get(0));	
+		((RemoteWebDriver) driver).findElementByXPath("//a[text()='Merge']").click();
+		Alert alert1 = driver.switchTo().alert();
+		alert1.accept();
+		Thread.sleep(20000);
+		String title1 = driver.getTitle();
+		System.out.println("Title of the page:"+title1);
+
+	}
+
+}
